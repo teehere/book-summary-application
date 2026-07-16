3bd38fb8784d539bbff9d9f0c4075fb74979a04b
teelexuan <teelexuan@gmail.com>
2026-07-16 22:44:46 +0800

    added readme

--- app.py
+++ app.py
@@ -2,29 +2,36 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel, RunnableLambda
+from langchain.agents import create_agent
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
 import streamlit as st
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-google_key = os.getenv('GOOGLE_API_KEY')
+google_key = os.getenv('google_key')
 os.environ['GOOGLE_API_KEY'] = google_key
 
 st.title('Book Summary Application')
 
-input_text = st.text_input("Search the book you want: ")
+input_book = st.text_input("Search the book you want: ")
+
+option = st.radio(
+    "Choose an action", ("Generate Summary", "Search by Wikipedia")
+)
 
 @st.cache_resource
 def load_llm(): 
     return ChatGoogleGenerativeAI(temperature=0.8, model='gemini-3-flash-preview', max_retries=3)
 llm = load_llm()
+parser = StrOutputParser()
 
 #1 - book summary
 first_prompt = PromptTemplate(
     input_variables=['book_name'],
     template="Provide a concise summary (100-150 words) of the book '{book_name}'."
 )
-parser = StrOutputParser()
 first_chain = first_prompt | llm | parser
 
 #2 - publication date
@@ -45,6 +52,7 @@ parallel_tasks = RunnableParallel(
     publication_date=second_chain,
     author_info=third_chain
 )
+
 full_chain = (
     first_chain 
     | RunnableLambda(
@@ -57,17 +65,52 @@ full_chain = (
     )
 )
 
-if input_text and st.button("Generate Summary"):
-    try: 
-        output = full_chain.invoke({"book_name": input_text})
+wiki = WikipediaQueryRun(
+    api_wrapper = WikipediaAPIWrapper() 
+)
 
-        st.subheader("Book Summary")
-        st.write(output["summary"])
+agent = create_agent(
+    model=llm,
+    tools=[wiki]
+)
 
-        st.subheader("Publication Date")
-        st.write(output["results"]["publication_date"])
+if input_book and st.button("Run"):
 
-        st.subheader("Author Info")
-        st.write(output["results"]["author_info"])
-    except Exception as e:
-        st.error(f"Error: {e}") 
+    if option == "Generate Summary":
+
+        try:
+            with st.spinner("Generating..."):
+                output = full_chain.invoke({"book_name": input_book})
+
+                st.subheader("Book Summary")
+                st.write(output["summary"])
+
+                st.subheader("Publication Date")
+                st.write(output["results"]["publication_date"])
+
+                st.subheader("Author Info")
+                st.write(output["results"]["author_info"])
+        except Exception as e:
+            st.error(f"Error: {e}") 
+    
+    else:
+        try:
+            with st.spinner("Searching Wikipedia..."):
+
+                response = agent.invoke(
+                    {
+                        "messages": [
+                            {
+                                "role": "user",
+                                "content": f"Tell me about the book {input_book} using Wikipedia."
+                            }
+                        ]
+                    }
+                )
+
+            st.subheader("Wikipedia Result")
+            st.write(response["messages"][-1].content)
+
+        except Exception as e:
+            st.error(f"Error: {e}") 
+                
